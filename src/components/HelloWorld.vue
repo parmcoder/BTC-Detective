27324e192f06ebadd6819a52a167fb95733c8690
parmcoder <possawat2017@gmail.com>
2021-04-03 17:53:59 +0700

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,87 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button v-if="done.length === 0" @click="getAnswer">Click</button>
+    <h5>progress: {{ done }}</h5>
+
+    <h1>Transactions</h1>
+
+    <h5>{{ answer }}</h5>
+
+    <h1>Balances</h1>
+
+    <h5>{{ balances }}</h5>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data: () => ({
+    answer: {},
+    balances: {},
+    done: ""
+  }),
+  methods: {
+    readProducts() {
+      console.log("Hello");
+    },
+    async getAnswer() {
+      this.done = "working!";
+
+      const address = "0xEcA19B1a87442b0c25801B809bf567A6ca87B1da";
+      var link =
+        "https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=";
+      var query =
+        "&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken";
+      var queue = [address];
+      var visited = new Set();
+      // var balances = [
+      //   { address: "0xEcA19B1a87442b0c25801B809bf567A6ca87B1da", balance: 0 }
+      // ];
+      var transactions = [];
+      while (queue.length > 0) {
+        const { data } = await axios.get(link + queue[0] + query);
+        // var transList = []
+        if (data.status == 1) {
+          const filtered = data.result.filter(a => a.tokenSymbol === "BKTC");
+          const formatted = filtered.map(a => {
+            return { hash: a.hash, to: a.to, from: a.from, amount: a.value };
+          });
+          formatted.forEach(item => {
+            if (transactions.filter(p => p.hash == item.hash).length === 0) {
+              transactions.push(item);
+              queue.push(item.to);
+            }
+          });
+          // console.log(transferTotal);
+          // console.log(queue.length);
+          visited.add(queue[0]);
+          queue.shift();
+          // console.log(transactions);
+          // console.log(filtered);
+          this.answer = Array.from(transactions);
+        }
+      }
+      let balances = Array.from(visited).map(a => {
+        return { address: a, balance: 0 };
+      });
+      // transactions.forEach(item => {
+      //   balances[indexOf]
+      // });
+      console.log("DONE!");
+      this.done = "done!";
+      this.balances = balances;
+
+      //Suppose we have all the transactions
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
